9e2257c27f211311614ccd88e2b6c1d3ec8a422e
cffly <130959872+cffly@users.noreply.github.com>
2023-05-29 14:01:41 +0800

    Update script.js

--- 111-cffly/dist/script.js
+++ 111-cffly/dist/script.js
@@ -1,1 +1,8 @@
+var count = 0;
+    var counterElement = document.getElementById("counter");
+
+    function increment() {
+      count+=2;
+      counterElement.textContent = count;}
+
 document.write("HELLO我是冷霏霏 ")
